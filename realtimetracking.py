ce6b54114a6a7b766f93a70e2f5242a809f45c25
Marie Joe Wakim <132767717+XxMJ14xX@users.noreply.github.com>
2024-02-25 19:32:42 +0200

    Update realtimetracking.py

--- realtimetracking.py
+++ realtimetracking.py
@@ -131,7 +131,7 @@ if len(detections):
     wb = 15;
     wp = 2;
     wt = 0;
-    print(detections.iloc[[:,3]])
+    print(detections.iloc[[0:4,3]])
     for c in np.unique(detections[:, -1]):
         n = (detections[:, -1] == c).sum()
         s += f"{n} {classes[int(c)]}{'s' * (n > 1)}, " 
